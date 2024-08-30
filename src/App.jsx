24286ef6f91d27691f8b1ad8a75443ea2041cb23
CristianF09 <cris7i_laurentiu@yahoo.com>
2024-08-30 19:37:19 +0300

    Update 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import Profile from './components/Profile/Profile';
-import Statistics from './components/Statistics/Statistics'; 
+import Statistics from './components/Statistics/Statistics';
 import FriendList from './components/FriendList/FriendList';
-import TransactionHistory from './components/TransactionHistory/TransactionHistory'; 
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
-import user from './data/user.json'; 
-import data from './data/data.json'; 
-import friends from './data/friends.json'; 
-import transactions from './data/transactions.json'; 
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
-const App = () => {
+function App() {
   return (
     <div>
-      <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </div>
   );
-};
+}
 
 export default App;
