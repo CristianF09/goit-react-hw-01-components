ef161642b4c2201cb4e5b6455cfa5f4459a9221c
CristianF09 <cris7i_laurentiu@yahoo.com>
2024-09-03 20:12:39 +0300

    Update Styling Profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -11,17 +11,16 @@ const Profile = ({ username, tag, location, avatar, stats }) => {
         <p className={styles.tag}>@{tag}</p>
         <p className={styles.location}>{location}</p>
       </div>
-
       <ul className={styles.stats}>
-        <li>
+        <li className={styles.statItem}>
           <span className={styles.label}>Followers</span>
           <span className={styles.quantity}>{stats.followers}</span>
         </li>
-        <li>
+        <li className={styles.statItem}>
           <span className={styles.label}>Views</span>
           <span className={styles.quantity}>{stats.views}</span>
         </li>
-        <li>
+        <li className={styles.statItem}>
           <span className={styles.label}>Likes</span>
           <span className={styles.quantity}>{stats.likes}</span>
         </li>
