e19ed00da3e811f19751d1613187661c17bab635
CristianF09 <cris7i_laurentiu@yahoo.com>
2024-09-03 19:55:43 +0300

    Update Friends Style

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './FriendListItem.module.css';
 
-const FriendListItem = ({ avatar, name, isOnline }) => {
-  return (
-    <li className={styles.item}>
-      <span className={isOnline ? styles.statusOnline : styles.statusOffline}></span>
-      <img className={styles.avatar} src={avatar} alt={`${name} avatar`} width="48" />
-      <p className={styles.name}>{name}</p>
-    </li>
-  );
-};
+const FriendListItem = ({ avatar, name, isOnline }) => (
+  <li className={styles.item}>
+    <span
+      className={`${styles.status} ${isOnline ? styles.statusOnline : styles.statusOffline}`}
+    ></span>
+    <img className={styles.avatar} src={avatar} alt={`${name} avatar`} width="48" />
+    <p className={styles.name}>{name}</p>
+  </li>
+);
 
 FriendListItem.propTypes = {
   avatar: PropTypes.string.isRequired,
